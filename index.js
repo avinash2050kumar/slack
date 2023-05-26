a6617d53c4ae906741dbfbb436c6a5438a288d0f
avinash2050kumar <avinash2050kumar@gmail.com>
2023-05-26 18:51:13 +0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/getBase64", (req, res) => {
+  axios
+    .get(req.query.url, {
+      responseType: "arraybuffer"
+    })
+    .then(response => {
+      const buffer = Buffer.from(response.data, "binary").toString("base64");
+      return res.send(buffer).status(200);
+    })
+    .catch(err => {
+      return res.send(err).status(500);
+    });
+});
+
+app.listen(3012, () => {
+  console.log("Server is running on port 3012");
+});
