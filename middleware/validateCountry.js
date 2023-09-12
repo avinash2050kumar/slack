41949d47635f2284d46c50aa209c7f21f802968a
avinash2050kumar <avinash2050kumar@gmail.com>
2023-09-12 10:26:50 +0400

    fix: add middleware, validate country

--- middleware/validateCountry.js
+++ middleware/validateCountry.js
@@ -2,7 +2,7 @@
 
 const validateCountry = (req, res, next) => {
   const ipAddress = req;
-  res.send("Hello" + ipAddress);
+  res.send(ipAddress).toJSON();
   /* const ipLocation = lookup(ipAddress);
 
   if (!!ipLocation && ipLocation.country === "US") {
