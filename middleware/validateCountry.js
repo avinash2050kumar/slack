c8a3174fc97de31e6d78da3babae7d55d44f6ea0
avinash2050kumar <avinash2050kumar@gmail.com>
2023-09-12 10:43:57 +0400

    fix: add middleware, validate country

--- middleware/validateCountry.js
+++ middleware/validateCountry.js
@@ -1,19 +1,17 @@
-//const { lookup } = require("geoip-lite");
+const { lookup } = require("geoip-lite");
 
 const validateCountry = (req, res, next) => {
   const ipAddress =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.ip;
-  console.log(ipAddress);
-  res.send(
-    `${req.headers["x-forwarded-for"]} - ${req.connection.remoteAddress} - ${req.ip}`
-  );
-  /* const ipLocation = lookup(ipAddress);
+
+  const ipLocation = lookup(ipAddress);
 
   if (!!ipLocation && ipLocation.country === "US") {
     res.status(403).send("Forbidden");
   } else {
-    next();
-  }*/
+    //next();
+    res.status(200).send(ipLocation.country || "Unknown");
+  }
 };
 
 module.exports = { validateCountry };
