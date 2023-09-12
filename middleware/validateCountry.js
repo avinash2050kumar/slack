4d525368f5fd69ce4c26b429857106a97426e8e6
avinash2050kumar <avinash2050kumar@gmail.com>
2023-09-12 13:06:26 +0400

    fix: add middleware, validate country

--- middleware/validateCountry.js
+++ middleware/validateCountry.js
@@ -1,35 +1,27 @@
-const maxmind = require("maxmind-db-reader");
-const lookup = maxmind.openSync("path/to/your/maxmind-db-file.mmdb");
+const axios = require("axios");
 
-/*const fetchIpLocation = async ipAddress => {
+const fetchIpLocation = async ipAddress => {
   try {
-    const response = await axios.get(`https://ipapi.co/${ipAddress}/json/`);
-    const response2 = await axios.get(
-      `https://ipinfo.io/widget/demo/${ipAddress}`
+    const response = await axios.get(
+      `http://ip-api.com/json/${ipAddress}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,proxy,query`
     );
-    return response2.data;
+
+    return response.data;
   } catch (e) {
     console.log(e);
   }
-};*/
+};
 
 const validateCountry = (req, res, next) => {
   const ipAddress = req.headers["x-forwarded-for"] || req.ip; //|| req.connection.remoteAddress || req.ip;
 
-  const ip = "8.8.8.8";
-  const result = lookup.get(ip);
-
-  console.log(result);
-
-  res.send(ip).status(200);
-
-  /*fetchIpLocation(ipAddress)
+  fetchIpLocation(ipAddress)
     .then(ipLocation => {
       res.status(200).send(ipLocation);
     })
     .catch(err => {
       res.send(err).status(500);
-    });*/
+    });
 };
 
 module.exports = { validateCountry };
